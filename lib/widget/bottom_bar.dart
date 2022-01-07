ebecb632ef860a8dc5b50063e375a446ff8302db
songyoungwoon <thdduddns98@naver.com>
2022-01-07 17:07:03 +0900

    first commit

---
+++ lib/widget/bottom_bar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Container(
+        height: 50,
+        child: TabBar(
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.white60,
+          indicatorColor: Colors.transparent,
+          tabs: [
+            Tab(
+              icon: Icon(
+                Icons.favorite,
+                size: 18,
+              ),
+              child:Text('Favorites'),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.search,
+                size: 18,
+              ),
+              child:Text('Search'),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.home,
+                size: 18,
+              ),
+              child:Text('Home'),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.account_circle,
+                size: 18,
+              ),
+              child:Text('MyPage'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widget/search_bar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Row(
+      children: [
+        Expanded(
+          child: Column(
+            children: [
+              Container(),
+
+              Container(),
+            ],
+          ),
+        ),
+      ],
+    ));
+  }
+}
