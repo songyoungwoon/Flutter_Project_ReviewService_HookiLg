8029f9adcb98ba00b4d2697a9872d72457635ee4
Kim HyeJin <WZNT_KHJ>
2022-01-10 09:59:39 +0900

    writeReviewPage

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widget/search_bar.dart';
-
+import '../page/writeReview_page.dart';
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -12,6 +12,22 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body:Center(
+        child: IconButton(
+          onPressed: (){
+            Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => writeReview()),
+                );
+          },
+          icon: Icon(Icons.auto_stories,
+          size: 40,),  
+          color: Colors.grey[600],
+        ),
+        
+        
+      ) 
+      
 
     );
   }
