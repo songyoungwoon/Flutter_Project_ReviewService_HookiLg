c961348773e95b97674287eae6dff44a9a46de8d
narlyeon <skgusy99@gmail.com>
2022-01-12 10:27:14 +0900

    nahyeon mypage 01

--- lib/screen/my_page.dart
+++ lib/screen/my_page.dart
@@ -1,111 +1,97 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fp_review_service_hookilg/widget/appbar_widget.dart';
+import 'package:fp_review_service_hookilg/widget/button_widget.dart';
+import 'package:fp_review_service_hookilg/widget/numbers_widget.dart';
+import 'package:fp_review_service_hookilg/widget/profile_widget.dart';
+import 'package:fp_review_service_hookilg/model/user.dart';
+import 'package:fp_review_service_hookilg/utils/user_preferences.dart';
 
-class MyPage extends StatelessWidget {
-  const MyPage({Key? key}) : super(key: key);
 
+class MyPage extends StatefulWidget {
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Column(
-          children:[
-            Container(
-              padding: EdgeInsets.only(top: 50),
-              child: CircleAvatar(
-                radius: 100,
-                backgroundImage: AssetImage('images/profile.jpg'),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 15),
-              child: Text(
-                'name',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
-                    color: Colors.pink[200]),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              width: 140,
-              height: 0.5,
-              color: Colors.white,
-            ),
+    final user = UserPreferences.myUser;
 
-            Container(
-              padding: EdgeInsets.only(top: 10),
-                child: Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('mbti'),
-                      SizedBox(width: 20,),
-                      Text('age'),
-                    ],
-                  ),
-                ),
-            ),
-
-            Container(
-              padding: EdgeInsets.fromLTRB(10, 5, 10, 0),
-              child: FlatButton(
-                onPressed: () {},
-                child: Container(
-                  color: Colors.black12,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.edit,
-                        color: Colors.white,
-                      ),
-                      SizedBox(
-                        width: 12,
-                      ),
-                      Text(
-                        '프로필 수정',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: FlatButton(
-                onPressed: () {},
-                child: Container(
-                  color: Colors.black12,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.edit,
-                        color: Colors.white,
-                      ),
-                      SizedBox(
-                        width: 12,
-                      ),
-                      Text(
-                        '프로필 수정',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
+    return Scaffold(
+      appBar: buildAppBar(context),
+      body: ListView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          Row ( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children :[
+          ProfileWidget(
+            imagePath: user.imagePath,
+            onClicked: () async {},
+          ),
+          const SizedBox(height: 24),
+          Column( crossAxisAlignment: CrossAxisAlignment.center,
+            children :[
+          const SizedBox(height: 2),
+          buildName(user),
+          const SizedBox(height: 10),
+          Center(child: buildProfileModifyButton()),
+          ]
+          ),
           ],
-        ),
+          ),
+          const SizedBox(height: 30),
+          NumbersWidget(),
+          const SizedBox(height: 24),
+          buildMyReviewButton(),
+          const SizedBox(height: 48),
+          buildAbout(user),
+
+        ],
       ),
     );
   }
+
+  Widget buildName(User user) => Column(
+        children: [
+          Text(
+            user.name,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            '나의 한줄 소개',
+            style: TextStyle(color: Colors.grey, fontSize: 15),
+          )
+        ],
+      );
+
+  Widget buildMyReviewButton() => ButtonWidget(
+        text: 'My Review',
+        isBold: false,
+        onClicked: () {},
+      );
+    Widget buildProfileModifyButton() => ButtonWidget(
+        text: '내 프로필 수정하기',
+        isBold: true,
+        onClicked: () {},
+      );
+
+  Widget buildAbout(User user) => Container(
+        padding: EdgeInsets.symmetric(horizontal: 48),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'About',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              user.about,
+              style: TextStyle(fontSize: 16, height: 1.4),
+            ),
+          ],
+        ),
+      );
 }
---
+++ lib/widget/button_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatelessWidget {
+  final String text;
+  final VoidCallback onClicked;
+  final bool isBold;
+  const ButtonWidget({
+    Key? key,
+    required this.text,
+    required this.isBold,
+    required this.onClicked,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shadowColor: Colors.pinkAccent[400],
+          //shape: StadiumBorder(),
+          onPrimary: Colors.pink.shade200,
+          primary: Colors.white,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 3)
+        ),
+        child: Text(text, style: TextStyle(fontWeight: isBold? FontWeight.bold:FontWeight.normal),),
+        onPressed: onClicked,
+      );
+}
