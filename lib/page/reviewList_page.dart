0f75e448cb9a8728ccafa657e1e9af61385f8c44
songyoungwoon <thdduddns98@naver.com>
2022-01-15 20:55:10 +0900

    reviewList : change ui

--- lib/page/reviewList_page.dart
+++ lib/page/reviewList_page.dart
@@ -6,8 +6,10 @@ import 'package:fp_review_service_hookilg/screen/Welcome/welcome_screen.dart';
 
 import 'package:fp_review_service_hookilg/screen/my_page.dart';
 import 'package:fp_review_service_hookilg/widget/profile_widget.dart';
+
 // import 'writeReview_page.dart';
 import 'writeReviewTest_page.dart';
+
 // import 'showReview_page.dart';
 import 'showReviewTest_page.dart';
 import '../screen/fristhome.dart';
@@ -43,6 +45,7 @@ class _reviewListState extends State<reviewList> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         title: Text(
           'Review Board',
           style: TextStyle(
@@ -92,26 +95,39 @@ class _reviewListState extends State<reviewList> {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: Text("loading"));
           }
+
           return ListView.builder(
             itemCount: snapshot.data.docs.length,
             itemBuilder: (ctx, index) {
+              //method
+              MaterialPageRoute gotoShowReview() {
+                return MaterialPageRoute(
+                    builder: (context) => showReviewTest(
+                        snapshot.data.docs[index]['review_title'],
+                        snapshot.data.docs[index]['date_time'],
+                        snapshot.data.docs[index]['review_content']));
+              }
+
+              // 리뷰 유저 정보 받아와야됨
               return Card(
-                shadowColor: Colors.pink[100],
-                elevation: 7,
-                margin: EdgeInsets.symmetric(vertical: 13, horizontal: 15),
+                elevation: 0,
+                margin: EdgeInsets.symmetric(vertical: 6, horizontal: 0),
                 child: Container(
                   child: Column(
                     children: [
+                      // user profile
                       Container(
                         padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
                         child: Row(
                           children: [
                             CircleAvatar(
-                                radius: 25,
-                                backgroundImage: NetworkImage(snapshot.data.docs[index]['imagePath']),
-                                backgroundColor: Colors.amber[600],),
+                              radius: 15,
+                              backgroundImage: NetworkImage(
+                                  snapshot.data.docs[index]['imagePath']),
+                              backgroundColor: Colors.amber[600],
+                            ),
                             Container(
-                              padding: EdgeInsets.fromLTRB(15, 0, 5, 5),
+                              padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
@@ -119,163 +135,92 @@ class _reviewListState extends State<reviewList> {
                                     snapshot.data.docs[index]['user_name'],
                                     style: TextStyle(
                                         fontWeight: FontWeight.w400,
-                                        fontSize: 22,
+                                        fontSize: 12,
                                         fontFamily: 'NanumBarun'),
                                   ),
                                   SizedBox(
-                                    height: 8,
+                                    height: 5,
                                   ),
                                   Text(
-                                    'level',
-                                    style: TextStyle(fontFamily: 'NanumBarun'),
+                                    'Lv',
+                                    style: TextStyle(
+                                        fontSize: 10, fontFamily: 'NanumBarun'),
                                   ),
                                 ],
                               ),
                             ),
                             Expanded(child: SizedBox()),
                             Container(
-                              child: Row(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
                                 children: [
+                                  Text(
+                                    snapshot.data.docs[index]['date_time'],
+                                    style: TextStyle(fontSize: 8),
+                                  ),
+                                  Padding(padding: EdgeInsets.all(5)),
                                   spoilerIstrue
                                       ? Row(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.center,
                                           children: [
                                             Icon(
                                               Icons.circle,
+                                              size: 6,
                                               color: Colors.red,
-                                              size: 10,
-                                            ),
-                                            SizedBox(
-                                              width: 2,
                                             ),
                                             Text(
                                               '스포포함',
-                                              style: TextStyle(
-                                                fontSize: 10,
-                                                fontWeight: FontWeight.w400,
-                                                //fontFamily: 'NanumBarun',
-                                              ),
-                                            ),
+                                              style: TextStyle(fontSize: 8),
+                                            )
                                           ],
                                         )
-                                      : Text(''),
-                                  SizedBox(width: 5),
-                                  endingAnalysisIstrue
-                                      ? Row(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.center,
-                                          children: [
-                                            Icon(
-                                              Icons.circle,
-                                              color: Colors.indigo,
-                                              size: 10,
-                                            ),
-                                            SizedBox(
-                                              width: 2,
-                                            ),
-                                            Text(
-                                              '결말해석',
-                                              style: TextStyle(
-                                                fontSize: 10,
-                                                fontWeight: FontWeight.w400,
-                                              ),
-                                            ),
-                                            SizedBox(width: 5),
-                                          ],
-                                        )
-                                      : Text(''),
+                                      : Container(),
                                 ],
                               ),
                             ),
                           ],
                         ),
                       ),
+
+                      // line
                       Container(
                         width: 350,
-                        height: 0.5,
-                        color: Colors.pink[200],
+                        height: 0.1,
+                        color: Colors.grey,
                       ),
-                      Container(
-                        padding: EdgeInsets.fromLTRB(10, 0, 5, 0),
-                        child: ListTile(
-                          title: Text(
-                            snapshot.data.docs[index]['review_title'],
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
-                          ),
-                          //subtitle: Text(snapshot.data.docs[index]['content']),
-                          trailing: Container(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Container(
-                                  width: 40,
-                                  height: 40,
-                                  child: IconButton(
-                                    icon: Icon(Icons.favorite),
-                                    color: Colors.pink[300],
-                                    iconSize: 25,
-                                    onPressed: () {},
-                                  ),
-                                ),
-                                Text(
-                                  "N",
-                                  style: TextStyle(
-                                      color: Colors.amberAccent[700],
-                                      fontSize: 11,
-                                      fontFamily: 'NanumBarun',
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                          ),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => showReviewTest(
-                                      snapshot.data.docs[index]['review_title'],
-                                      snapshot.data.docs[index]['date_time'],
-                                      snapshot.data.docs[index]
-                                          ['review_content'])),
-                            );
-                          },
-                        ),
-                      ),
-                      Padding(padding: EdgeInsets.only(top: 10)),
-                      Container(
-                        // alignment: Alignment.topLeft,
-                        padding: EdgeInsets.fromLTRB(25, 0, 15, 15),
 
-                        child: Row(
-                          children: [
-                            Container(
-                              width: 180,
-                              height: 150,
-                              child: Text(
-                                  snapshot.data.docs[index]['review_brief']),
+                      // review title
+                      Container(
+                          padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                          alignment: Alignment.centerLeft,
+                          child: TextButton(
+                            child: Text(
+                              snapshot.data.docs[index]['review_title'],
+                              style: TextStyle(fontSize: 15),
                             ),
-                            SizedBox(width: 10),
-                            Container(
-                              alignment: Alignment.centerLeft,
-                              width: 132,
-                              height: 150,
-                              decoration: BoxDecoration(color: Colors.indigo[50]
-                                  /*
-                                  image: DecorationImage(
-                                    image: NetworkImage(
-                                        'https://w.namu.la/s/6542d430faf483de81317c1ad99df450eef28b9409d0adbc02e17ca5c72c99a45ea7435a0b335a109f81d2f169867cec26207c7a2a62d2d216457f17d19735a681b4b9ba7b8ce0fa6407ef5e77928495'),
-                                    fit: BoxFit.cover),
-                                */
-                                  ),
+                            onPressed: () {
+                              Navigator.push(context, gotoShowReview());
+                            },
+                          )),
+
+
+                      // review brief
+                      Container(
+                          padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                          alignment: Alignment.centerLeft,
+                          child: TextButton(
+                            child: Text(
+                              snapshot.data.docs[index]['review_brief'],
+                              style:
+                                  TextStyle(fontSize: 13, color: Colors.black),
                             ),
-                          ],
-                        ),
-                      ),
+                            onPressed: () {
+                              Navigator.push(context, gotoShowReview());
+                            },
+                          )),
+
+
+                      Padding(padding: EdgeInsets.only(top: 10)),
+                      // review image
                     ],
                   ),
                 ),
@@ -320,8 +265,7 @@ class _reviewListState extends State<reviewList> {
                           context,
                           MaterialPageRoute(
                               // builder: (context) => writeReview(movie_title, movie_director)),
-                              builder: (context) =>
-                                  WelcomeScreen()),
+                              builder: (context) => WelcomeScreen()),
                         );
                       },
                     ),
